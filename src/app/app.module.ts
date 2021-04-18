d8ea7c36c4da248d395b919d3fa0125e3dd7fc72
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2021-04-18 18:38:34 -0300

    build: install angular material suite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './src/components/header/header.component';
 import { HomesComponent } from './src/components/homes/homes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomesComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
