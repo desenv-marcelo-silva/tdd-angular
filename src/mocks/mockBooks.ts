314854e3a9e7a277a5773371e932c3d3b09c8e84
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2021-04-18 20:50:30 -0300

    refactor: change mock files
    Add price to json data

---
+++ src/mocks/mockBooks.ts
@@ -0,0 +1,20 @@
+export const homes = [
+  {
+    title: 'Home 1',
+    image: 'assets/listing.jpg',
+    location: 'New York',
+    price: '125',
+  },
+  {
+    title: 'Home 2',
+    image: 'assets/listing.jpg',
+    location: 'Boston',
+    price: '225',
+  },
+  {
+    title: 'Home 3',
+    image: 'assets/listing.jpg',
+    location: 'Chicago',
+    price: '335',
+  },
+];
